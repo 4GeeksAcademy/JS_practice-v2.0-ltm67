36401062467eb1be221bfe4dc7201df686494e1a
José Borges <110873863+BorgesJos@users.noreply.github.com>
2026-01-05 15:54:07 +0000

    second commit

--- main.js
+++ main.js
@@ -2,13 +2,29 @@ console.log("Hola desde la consola")
 
 
 console.log("Con un loop")
-let characters = ["Mario","Luigi","Bowser","Toad","Peach","Star"]
+let personajes = ["Mario","Luigi","Bowser","Toad","Peach","Star"]
 
-for (i = 0; i < characters.length; i++){
-    console.log(characters[i])
+for (i = 0; i < personajes.length; i++){
+    console.log(personajes[i])
 }
 
-for (i = characters.length - 1; i >= 0; i--){
-    console.log(characters[i])
+for (i = personajes.length - 1; i >= 0; i--){
+    console.log(personajes[i])
 }
+
+let personajesbuenos = []
+
+for (i = 0; i < personajes.length; i++){
+    if(personajes[i] !== "Bowser"){
+         personajesbuenos.push(personajes[i])
+    } 
+}
+
+console.log(personajes)
+console.log(personajesbuenos)
+
+console.log(personajes.filter((personaje) => personaje != "Bowser"))
+console.log(personajes)
+
+
 
