109b72d65967b24e36be57ccb1f0f46b4ce71209
José Borges <110873863+BorgesJos@users.noreply.github.com>
2026-01-05 04:35:34 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Hola desde la consola")
+
+
+console.log("Con un loop")
+let characters = ["Mario","Luigi","Bowser","Toad","Peach","Star"]
+
+for (i = 0; i < characters.length; i++){
+    console.log(characters[i])
+}
+
+for (i = characters.length - 1; i >= 0; i--){
+    console.log(characters[i])
+}
+
